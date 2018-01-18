11c6349b2138f1997474c875cd4574d3cfbb207f
Andy Grossberg and Michael Albers <andy@>
2018-01-18 11:38:35 -0800

    move the consonant to the end of the word if it starts with a consonant.

--- README.md
+++ README.md
@@ -27,23 +27,23 @@ _The following specs are included in the program:_
 
 * Take a sentence from the user, translate it into pig latin, and display it back to them.
 
-* The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels.
+* The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels. - X
   - Example Input: 3
   - Example Output: 3
 
-* The program adds "way" to single-letter words beginning with a vowel.
+* The program adds "way" to single-letter words beginning with a vowel. - X
   - Example Input: i
   - Example Output: iway
 
-* The program adds "ay" to a single-letter word beginning with a consonant
+* The program adds "ay" to a single-letter word beginning with a consonant - X
  - Example Input: c
  - Example Output: cay
 
-* The program adds "way" to a multi-letter word beginning with a vowel.
+* The program adds "way" to a multi-letter word beginning with a vowel. - X (implicit in step 2)
  - Example In: apple
  - Example Out: appleway
 
-* The program moves the consonant to the end of the word if it starts with a consonant
+* The program moves the consonant to the end of the word if it starts with a consonant - X
  - Example In: Banana
  - Example Out: ananaB
  - Exception: Chin (would become hinC)
@@ -54,7 +54,7 @@ _The following specs are included in the program:_
 
 * The program moves consecutive consonants to the end of the word
  - Example In: Chin
- - Example Out: inCh
+ - Example Out: hinC
 
 * The program adds "ay" to a multi-letter word that has just gotten consecutive consonants added at the end.
  - Example In: inCh
--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,48 @@
   var word = "";
 
 // business logic
+
 function addWayToVowel(mySentence){
    var x = mySentence.slice(0, 1);
    alert ("X = " + x);
    x = x.toUpperCase();
    if ( (x === "A") || (x === "E") || (x === "I") || (x === "O") || (x === "U") ) {
      alert ("X is a vowel!");
-     x = mySentence + "way";
+     return mySentence + "way";
    }
-   return x; // send the sentence back
+   //  return x; // send the sentence back
+   // problem here is it returned X in upperCase if mySentence ISN'T a vowel
+   // fixed it to return a value only if the vowel condition is true
+ }
+
+ function moveConsonantToEnd(mySentence){
+    var x = mySentence.slice(0, 1);
+    alert ("X = " + x);
+    var y = x.toUpperCase();
+    if ( ! ( (y === "A") || (y === "E") || (y === "I") || (y === "O") || (y === "U") ) ) {
+      alert("Y is a consonant!");
+      return mySentence.slice(1,mySentence.length) + x;
+      // return mySentence + "ay"; // send the sentence back with 'ay' at the end
+    }
+  }
+
+
+ // function addAyToConsonant(mySentence){
+ //    var x = mySentence.slice(0, 1);
+ //    alert ("X = " + x);
+ //    var y = x.toUpperCase();
+ //    if ( (y === "A") || (y === "E") || (y === "I") || (y === "O") || (y === "U") ) {
+ //
+ //      alert("Y is a consonant!");
+ //      alert("With slices: " + mySentence.slice(1,mySentence.length));
+ //      return mySentence.slice(1,mySentence.length) + x + "ay";
+ //    }
+ //    return x; // send the sentence back
+ //  }
+
+ function parseSentence(mySentence){
+   words = mySentence.split(" ");
+   alert("words = "+ words);
  }
 
 // user interface logic
@@ -20,8 +53,10 @@ $(document).ready(function() {
   $("form#sentence-form").submit(function(event) {
     event.preventDefault();
     sentence = $('#sentence').val();
-    sentence = addWayToVowel(sentence); // changes sentence by adding "way" to the end if it starts with vowel
+    // sentence = addWayToVowel(sentence); // changes sentence by adding "way" to the end if it starts with vowel
+    sentence = moveConsonantToEnd(sentence); // changes sentence by adding "ay" to the end if it starts with consonant
 
+  //  parseSentence(sentence); // doesn't have a return value yet
 
 
     $('#output').text(sentence);
